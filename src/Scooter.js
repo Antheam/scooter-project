1b46c0e1c3365cacb2e0460a4549f939ef97c11b
antheam <anthea.merton@gmail.com>
2022-10-21 15:14:19 +0100

    Added attributes and methods to Scooter class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,41 @@
 class Scooter{
-  // code here
+ 
+  constructor(station, user, serial, charge, isBroken = false, docked = true){
+    this._station = station
+    this._user = user
+    this.serial = Math.floor(Math.random() * 10000)
+    this._charge = charge
+}
+
+  get station(){
+
+  }
+rent(){
+  if(this.isBroken = false && this.charge >20){
+    throw new Error("Scooter low on battery, please charge.")
+  }
+}
+
+dock(station){
+  this.station = station
+  if(this.station == ""){
+    throw new Error("Docking station required!")
+    this.docked = true
+    this.user = ""
+  }
+}
+async recharge(){
+  this.charge = 100
+}
+
+
+async requestRepair(){
+  this.Broken = false
+
+ await new Promise(resolve => setTimeout(resolve, 2000)); // wait 2 seconds
+
+}
+
 }
 
 
